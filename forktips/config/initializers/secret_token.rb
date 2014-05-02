5713e13fe219e228f4f531ad8d6e9f12d19e799a
Gregg Horton <greggahorton@gmail.com>
2014-05-01 19:33:38 -0700

    adding rails project

---
+++ forktips/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forktips::Application.config.secret_key_base = 'f149332b513471d94a63e00c5d94f21049e1170e5daaeaa10384c38bbdf9349b07520f1e7a93c4cc921244aaacae18f94d46b4ab714d10f54e52f48cd902bec7'
